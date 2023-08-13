b9f03e14917a1223e84e0582bc4bbb1451916069
Sam McKelvie <dev@mckelvie.org>
2023-08-13 10:44:41 -0700

    TCP connection timeouts and retries

--- jvc_projector/constants.py
+++ jvc_projector/constants.py
@@ -18,10 +18,10 @@ IDLE_DISCONNECT_TIMEOUT = 2.0
 """For autoconnect transports, the timeout for the client to disconnect after an idle period,
    in seconds."""
 
-CONNECT_TIMEOUT = 10.0
+CONNECT_TIMEOUT = 15.0
 """The timeout for connecting to the projector over TCP/IP, in seconds."""
 
-CONNECT_RETRY_INTERVAL = 0.5
+CONNECT_RETRY_INTERVAL = 1.0
 """The interval between connection attempts over TCP/IP, in seconds."""
 
 # Initial connection handshake:
